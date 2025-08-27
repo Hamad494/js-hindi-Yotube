ae15908eaf844fd99cbf41195f485110cdd97a93
Hamad Mustafa <157937270+Hamad494@users.noreply.github.com>
2025-08-27 07:29:28 +0000

    dataTypes

--- 01_Basic/01_variables.js
+++ 01_Basic/01_variables.js
@@ -1,9 +1,13 @@
-  const accountId = 12345
-  let accountEmail = "hamadmustafa@1234"
-  var accountPassword = "1234"
-  accountCity = "Lahore"
+const accountId = 12345
+let accountEmail = "hammad494@gmail.com"
+var accountPassword = "23452"
+accountCity = "Muzaffarabad"
 
-  accountId = 2
+//accountId = 23 //not allowed to chnage
+accountEmail = "Mustafa234@gmail.com"
+accountPassword = "342345"
+accountCity = "Islamabad"
+let accountState;
+console.log(accountId);
 
-  console.log(accountId);
-  
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
