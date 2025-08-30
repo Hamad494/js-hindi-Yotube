dfa195faf5cbd36b0e1947c7c02dd4d29040eb6b
Hamad Mustafa <157937270+Hamad494@users.noreply.github.com>
2025-08-30 13:11:27 +0000

    data types

--- 01_Basic/02_dataTypes.js
+++ 01_Basic/02_dataTypes.js
@@ -1,3 +1,19 @@
 "use strict"; // treat all js code as newer version
 
 // alert(3 + 3) //we are using node js . not browser
+
+let name = "Hammad"    // string data type
+let age  = 18   // intiger data type
+let isLoggedIn = true  // bolean data type
+
+// number => 2 power of 53
+// bigint
+// string =>
+// boolean => true/false
+// null standalone value
+// undefined =>
+// Symbol => unique
+
+// object
+
+console.log(typeof null   )
