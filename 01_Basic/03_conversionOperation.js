b51fdcde88b831d845ef1fea0e0bfb9115315331
Hamad Mustafa <157937270+Hamad494@users.noreply.github.com>
2025-09-01 05:05:33 +0000

    Conversion

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = true
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=>0
