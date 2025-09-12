f8b7b6fcca8d73f55d0c0ec8903fd339eb5f6a0d
Hamad Mustafa <157937270+Hamad494@users.noreply.github.com>
2025-09-12 06:21:52 +0000

    Conversion of data Type

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -1,12 +1,27 @@
 let score = true
 
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
 let ValueInNumber = Number(score)
-console.log(typeof ValueInNumber)
-console.log(ValueInNumber)
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false=>0
+
+let IsLoggedIn = "Hammad"
+
+let BoolianIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(BoolianIsLoggedIn);
+
+ // 1=> true    0 => false
+ // "" => false  
+ //"Hammad" => true   
+
+ let SomeNumber = 33
+
+ let StringNumber = String(SomeNumber)
+ console.log(StringNumber);
+ console.log(typeof(StringNumber));
