b8fc2faf38f804a12722d802396cb30b580f2889
Hamad Mustafa <157937270+Hamad494@users.noreply.github.com>
2025-10-16 15:57:18 +0000

    changes

--- 01_Basic/05_string.js
+++ 01_Basic/05_string.js
@@ -3,4 +3,47 @@ const repocount = 50
 
 //console.log(name + " value " + repocount)
 
-console.log('My name is ${name} and my repo count is ${repocount} ');
+
+
+console.log(`Hi My name is ${name} and my Repo Count is ${repocount}`)
+
+const gameName = new String('Hammad-Mustafa-com');
+console.log(gameName[0]);
+console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('a'))
+
+const newString = gameName.substring(-8, 4);
+   
+//console.log(newString);
+
+const anotherString = gameName.slice(-7, 4)
+//console.log(anotherString);
+
+const newstringOne = "  hammad "
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+
+const url = "https:\\hammad.com/hammad%20Mustafa";
+//console.log(url.replace('%20', '-'));
+//console.log(url.includes('hammad'));
+
+//console.log(gameName.split('-'));
+
+
+const Sname = "Aslam"
+const age = 18
+
+console.log(`My Name is ${Sname} and my age is ${age} .`);
+
+console.log(gameName.bold());
+console.log(gameName.blink());
+console.log(gameName.fontcolor('Orange'));
+console.log(gameName.charAt(4));
+
+const Mark = "ABC"
+console.log(Mark.charCodeAt(1));  
