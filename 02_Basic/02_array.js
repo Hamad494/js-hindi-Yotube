8fee73af55967165051bb08123cd0dd080b2d986
Hamad Mustafa <157937270+Hamad494@users.noreply.github.com>
2025-10-28 13:33:37 +0000

    Arrays

---
+++ 02_Basic/02_array.js
@@ -0,0 +1,12 @@
+const marvel_heroes = ["thor", "Ironman", "spiderman" ]
+const dc_heroes= ["superman", "flash", "batman"]
+
+ // marvel_heroes.push(dc_heroes)
+
+//  console.log(marvel_heroes);
+//  console.log(marvel_heroes[3][1]);
+
+const AllHeroe = marvel_heroes.concat(dc_heroes)
+console.log(AllHeroe);
+
+ 
