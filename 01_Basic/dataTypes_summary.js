62c2946011f791999f99cca6271f555b46bd676a
Hamad Mustafa <157937270+Hamad494@users.noreply.github.com>
2026-02-10 11:39:55 +0000

    practice

--- 01_Basic/dataTypes_summary.js
+++ 01_Basic/dataTypes_summary.js
@@ -1,58 +1,64 @@
-// # Primitives 
+// // # Primitives 
 
-// 7 dataTypes: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// // 7 dataTypes: String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
-const Score = 100
-const scoreValue = 100.3
+// const Score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outSideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail;
 
-const id = Symbol("123")
-const anotherId = Symbol("123")
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-const bigNumber = 4323456678765432345678766554n
-console.log(typeof(bigNumber));
+// const bigNumber = 4323456678765432345678766554n
+// console.log(typeof(bigNumber));
 
 
-// #Refrence type (Non-Primitives)
+// // #Refrence type (Non-Primitives)
 
-// Array, Objects, Functions
+// // Array, Objects, Functions
 
-const heroes = ["Hammad", "Nadeem", "Amjad"];
-// let myObj = {
-//       name: hammad,
-//       age: 22,
+// const heroes = ["Hammad", "Nadeem", "Amjad"];
+// // let myObj = {
+// //       name: hammad,
+// //       age: 22,
 
+// // }
+
+// const myFunction = function() {
+//     console.log("Hello World");
 // }
 
-const myFunction = function() {
-    console.log("Hello World");
-}
-
-console.log(typeof(anothertype))
+// console.log(typeof(anothertype))
 
 
-///// *************************************8
+// ///// *************************************8
 
-// stack(paramitives) , Heap(non-paramitives)
+// // stack(paramitives) , Heap(non-paramitives)
 
-let name = "HammadKashmiri494"
+// let name = "HammadKashmiri494"
 
-let anotherName = name
-anotherName = "HammadMustafa"
+// let anotherName = name
+// anotherName = "HammadMustafa"
 
-// console.log(name);
-// console.log(anotherName);
+// // console.log(name);
+// // console.log(anotherName);
 
-let userOne = {
-    email: "user@google.com",
-    easypay: "03179023494"
-}
+// let userOne = {
+//     email: "user@google.com",
+//     easypay: "03179023494"
+// }
 
-let userTwo = userOne
+// let userTwo = userOne
 
-userTwo.email= "hammad@google.com"
+// userTwo.email= "hammad@google.com"
+
+
+
+
+
+
